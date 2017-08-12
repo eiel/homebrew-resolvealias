17982a3b2b394bfd4140f11da4f203ac4ca3af36
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-12 11:09:06 +0900

    v0.0.3

--- Formula/resolvealias.rb
+++ Formula/resolvealias.rb
@@ -1,30 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Resolvealias < Formula
-  desc "Resolve file path from alias on Mac"
+  desc "Resolve file path from alias of mac"
   homepage "https://github.com/eiel/ResolveAlias"
-  url "https://github.com/eiel/ResolveAlias/releases/download/v0.0.2/ResolveAlias.zip"
-  sha256 "814508eb5a0ed8cea68983c1fc768fe38c62449da26b16585646ae5e4976c0e9"
+  url "https://github.com/eiel/ResolveAlias/releases/download/v0.0.3/ResolveAlias.gz"
+  sha256 "b6d53813db7a2e42eafd34b9b4ee9978d5099392e818b62e3ed02dbf5ccd16c8"
 
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     bin.install "ResolveAlias"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ResolveAlias`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
